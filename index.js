bfb50165bba33d54d6af54404eab00ab0578daec
Lucas Gonçalves <l.paulon97@gmail.com>
2018-08-18 14:51:38 -0300

    Change function name

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const bot = new Composer()
 bot.use(log())
 bot.use(session())
 
-const mock = (s) => s.split('').map((c) => (Math.random() > 0.5) ? c : c.toUpperCase()).join('');
+const spongeMock = (s) => s.split('').map((c) => (Math.random() > 0.5) ? c : c.toUpperCase()).join('');
 
-bot.hears(/\/dingus (.+)/i, (ctx) => ctx.reply(mock(ctx.match[1])));
+bot.hears(/\/spongemock (.+)/i, (ctx) => ctx.reply(spongeMock(ctx.match[1])));
 
 module.exports = bot
